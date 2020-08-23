d68f6ad139b957ad9963c02e9ed9062aacd7d9cc
yair suberri <yair191003@gmail.com>
2020-08-24 00:01:17 +0300

    init

---
+++ Gym/FormLogin.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace Gym
+{
+    partial class FormLogin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.checkButton = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.password = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.idNumber = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // checkButton
+            // 
+            this.checkButton.Location = new System.Drawing.Point(92, 175);
+            this.checkButton.Margin = new System.Windows.Forms.Padding(2);
+            this.checkButton.Name = "checkButton";
+            this.checkButton.Size = new System.Drawing.Size(91, 19);
+            this.checkButton.TabIndex = 9;
+            this.checkButton.Text = "כניסה";
+            this.checkButton.UseVisualStyleBackColor = true;
+            this.checkButton.Click += new System.EventHandler(this.CheckButtonClick);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(194, 128);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(40, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "סיסמה";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // password
+            // 
+            this.password.Location = new System.Drawing.Point(52, 128);
+            this.password.Margin = new System.Windows.Forms.Padding(2);
+            this.password.Name = "password";
+            this.password.Size = new System.Drawing.Size(131, 20);
+            this.password.TabIndex = 7;
+            this.password.UseSystemPasswordChar = true;
+            this.password.TextChanged += new System.EventHandler(this.password_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(194, 78);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(54, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "מספר ת\"ז";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // idNumber
+            // 
+            this.idNumber.Location = new System.Drawing.Point(52, 78);
+            this.idNumber.Margin = new System.Windows.Forms.Padding(2);
+            this.idNumber.Name = "idNumber";
+            this.idNumber.Size = new System.Drawing.Size(131, 20);
+            this.idNumber.TabIndex = 5;
+            this.idNumber.TextChanged += new System.EventHandler(this.idNumber_TextChanged);
+            // 
+            // FormLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.checkButton);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.password);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.idNumber);
+            this.Name = "FormLogin";
+            this.Text = "FormLogin";
+            this.Load += new System.EventHandler(this.FormLogin_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button checkButton;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox password;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox idNumber;
+    }
+}
---
+++ Gym/FormLogin.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.OleDb;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Gym
+{
+    public partial class FormLogin : Form
+    {
+        private OleDbConnection dataConnection;
+        public FormLogin()
+        {
+            InitializeComponent();
+            OpenDb();
+        }
+        private void OpenDb()
+        {
+            dataConnection = new OleDbConnection();
+            try
+            {
+                dataConnection.ConnectionString = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source=C:\\Projects_2021\\Project_YairSuberri\\Access\\dbGym.mdb";
+                dataConnection.Open();
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show("Error accessing the database: " + e.Message, "Errors", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+        private void FormLogin_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void password_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void idNumber_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void CheckButtonClick(object sender, EventArgs e)
+        {
+            String line, password;
+            int id;
+            try
+            {
+                OleDbCommand datacommand = new OleDbCommand();
+                datacommand.Connection = dataConnection;
+                datacommand.CommandText = " SELECT        employID, employPassword " +
+                                           "FROM            tblEmployees " +
+                                           "WHERE        employID= " + this.idNumber.Text;
+                OleDbDataReader dataReader = datacommand.ExecuteReader();
+                dataReader.Read();
+                id = dataReader.GetInt32(0);
+                password = dataReader.GetString(1);
+                if (password == this.password.Text)
+                {
+                    this.Hide();
+                    FormMenu frMenu = new FormMenu();
+                    frMenu.Show();
+                    frMenu.Disposed += new EventHandler(frMenu_Disposed);
+                }
+                else
+                {
+                    line = "invalid password: " + this.password.Text;
+                    MessageBox.Show(line, "Error");
+                }
+            }
+            catch (Exception err)
+            {
+                line = "invalid id: " + this.idNumber.Text + err.Message;
+                MessageBox.Show(line, "Error");
+            }
+        }
+        void frMenu_Disposed(object sender, EventArgs e)
+        {
+            this.idNumber.Text = "";
+            this.password.Text = "";
+            this.Show();
+            this.Activate();
+        }
+    }
+}
